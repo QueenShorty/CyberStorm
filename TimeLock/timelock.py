d7e469a8cc9f446c5ab318c8c94903e744577002
Riddler6897 <bradforddoughty@gmail.com>
2019-04-16 11:37:45 -0500

    Got started on timelock
    So far the program spits out the correct time in seconds. Not sure how to handle subtracting an hour because of Daylight Savings Time. Havent started on hash stuff

--- TimeLock/timelock.py
+++ TimeLock/timelock.py
@@ -6,3 +6,19 @@
 ####################
 
 #Github link: https://github.com/QueenShorty/CyberStorm/edit/WorkingInProgress/TimeLock/timelock.py
+import time
+import datetime
+import hashlib
+
+#Example dates he gave on PDF
+x = '2013 05 06 06 43 25 UTC' #Had to change the hour from 07 to 06 to get correct result. Not sure how to make it go back an hour if in Daylight Savings Time
+y = '1999 12 31 23 59 59 UTC' #Didnt have to change this one since it is on normal time
+
+def dates(s): #Takes string date and converts it to an integer date
+    return datetime.datetime.strptime(s, '%Y %m %d %H %M %S %Z') #Just formatting stuff
+
+start = dates(y) #Saves the earliest date here
+end = dates(x) #Saves the most recent date here
+
+delta = end - start #Subtracts both dates
+print(delta.total_seconds()) #Prints out the subtracted dates in seconds
