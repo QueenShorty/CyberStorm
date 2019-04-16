8f3ef73fedb4e8f9d2f03eca4bcf6bbe28d75c40
Destany <destanymarie1997@gmail.com>
2019-04-16 10:19:52 -0500

    created folders and defualt files

---
+++ TimeLock/timelock.py
@@ -0,0 +1,8 @@
+####################
+# The Athenians    #
+# CSC 442/CYEN 301 #
+# TimeLock         #
+# April 16, 2019   #
+####################
+
+#Github link: https://github.com/QueenShorty/CyberStorm/tree/WorkingInProgress
