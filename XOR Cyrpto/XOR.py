cb701cddf3494f4db133b96a6bfa04c8d5b32bbc
jlg077 <32622018+jlg077@users.noreply.github.com>
2019-04-16 19:36:39 -0500

    Update XOR.py

--- XOR Cyrpto/XOR.py
+++ XOR Cyrpto/XOR.py
@@ -6,3 +6,30 @@
 ####################
 
 #Github link: https://github.com/QueenShorty/CyberStorm/edit/WorkingInProgress/XOR%20Cyrpto/XOR.py
+import sys
+import fileinput
+
+def xor():
+        m = []#holds plaintext/ciphertext
+
+    with open("key", "r") as file:#reads the file, key
+          k = []#will hold the key
+          for line in file:
+          k.append(line)
+
+        for line in fileinput.input():#read from stdin to grab ciphertext/plaintext
+          pass
+        a = len(m)
+        b = len(k)
+
+        if a == b:#if the key and message are the same length, xor them bit by bit
+          bitxor = m ^ k
+          print bitxor
+              
+        elif a < b:#if the key is longer, cut off enough elements so it is the same length as the message
+          bitxor = m ^ k[0:a]
+          print bitxor
+
+        #if the message is longer,keep looping through the key until you've gone through all of the message bits
+
+        else:
