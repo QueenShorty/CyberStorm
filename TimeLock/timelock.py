cc82571af1579543589b4d20ab21620c048e6719
Riddler6897 <bradforddoughty@gmail.com>
2019-04-20 15:08:54 -0500

    Final working version for timelock.py
    Figured out DST, now takes epoch time from stdin, and now gets current time. Got the correct answer in every test case.

--- TimeLock/timelock.py
+++ TimeLock/timelock.py
@@ -9,38 +9,35 @@
 import time
 import datetime
 import hashlib
+import sys
 
-#Example dates he gave on PDF
-x = '2013 05 06 06 43 25' #Had to change the hour from 07 to 06 to get correct result. Not sure how to make it go back an hour if in Daylight Savings Time
-y = '1999 12 31 23 59 59' #Didnt have to change this one since it is on normal time
+current = datetime.datetime.now() #Gets the current time
+epoch = raw_input() #Gets the epoch time from stdin
 
-def dates(s): #Takes string date and converts it to an integer date
-    return datetime.datetime.strptime(s, '%Y %m %d %H %M %S') #Just formatting stuff
+def UTC(s): #Converts time from DST to UTC
+    secs = time.mktime(s)
+    r = time.gmtime(secs)
+    return time.mktime(r) #Returns UTC in seconds for easier math
 
-start = dates(y) #Saves the earliest date here
-end = dates(x) #Saves the most recent date here
+date = datetime.datetime.strptime(epoch, '%Y %m %d %H %M %S') #Formats from string to date
+start = UTC(date.timetuple()) #Converts to tuple, then will return as seconds in UTC form
+end = UTC(current.timetuple()) #Converts to tuple, then will return as seconds in UTC form
 
-delta = end - start #Subtracts both dates
-x = delta.total_seconds() #Stores the total number of seconds in x
+final = end - start #Subtracts both dates
 
-print(x) #JUST FOR TESTING
+if final % 60 != 0: #Checks to see if the number of seconds is divisible by 60
+    final = final / 60 #Divides the number by 60
+    final = int(final) #Gets rid of decimal places
+    final = final * 60 #Multiplies this result by 60 to get a number within the 60 second interval
+else:
+    final = int(final) #Gets rid of decimal place if divisible by 60
 
-#We probably need to optimize this
-if x % 60 != 0: #Checks to see if the number of seconds is divisible by 60
-    x = x / 60 #Divides the number by 60
-    x = int(x) #Gets rid of decimal places
-    x = x * 60 #Multiplies this result by 60 to get a number within the 60 second interval
-
-print(x) #JUST FOR TESTING
-
-test = hashlib.md5(str(hashlib.md5(str(x)).hexdigest())).hexdigest() #MD5 the number of seconds twice to get hash code
-
-print(test) #JUST FOR TESTING
+md5 = hashlib.md5(str(hashlib.md5(str(final)).hexdigest())).hexdigest() #MD5 the number of seconds twice to get hash code
 
 Alpha = "" #Will hold all the letters in hash
 Num = "" #Will hold all the numbers in hash
 
-for i in test: #Goes through the hash code
+for i in md5: #Goes through the hash code
     if i.isalpha() == True: #Checks if each character in hash is a letter
         Alpha += i #Adds letters to Alpha
     else:
@@ -50,3 +47,4 @@ for i in test: #Goes through the hash code
 Answer = Alpha[:2] + Num[-1] + Num[-2] #Combines the first two letters with the last two numbers
 
 print(Answer)
+
