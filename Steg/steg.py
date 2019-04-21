82fd1c0ec41e83e6aa830037c4f9d427db727d73
Destany <destanymarie1997@gmail.com>
2019-04-21 15:06:29 -0500

    set up for steg

---
+++ Steg/steg.py
@@ -0,0 +1,9 @@
+####################
+# The Athenians    #
+# CSC 442/CYEN 301 #
+# Steg             #
+# April 21, 2019   #
+####################
+
+#Github link: https://github.com/QueenShorty/CyberStorm/edit/WorkingInProgress/
+
