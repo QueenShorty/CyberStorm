91e48096e7cdf33654ad8ff7a05c094a0639bb53
Destany <destanymarie1997@gmail.com>
2019-04-23 10:33:00 -0500

    laying out the foundation

--- Steg/steg.py
+++ Steg/steg.py
@@ -7,3 +7,25 @@
 
 #Github link: https://github.com/QueenShorty/CyberStorm/edit/WorkingInProgress/
 
+#Example: python steg.py -(bB) -(sr) -o<val> -w<val> [-h<val>]
+
+#-b Use the bit method
+#-B Use the byte method
+#-s Store (and hide) data
+#-r Retrieve hidden data
+#-o<val> Set offset to <val>
+#-i<val> Set interval to <val>
+#-w<val> Set wrapper file to <val>
+#-h<val> Set hidden file to <val>
+
+
+from math import *
+import sys
+
+method = sys.argv[1]
+data = sys.argv[2]
+offset = sys.argv[3]
+interval = sys.argv[4]
+
+#store from left to right : but for cyberstorm might be right to left
+
