c4528a914330fa736be56fa70c5bae954d676870
bdoughty <bradforddoughty@gmail.com>
2019-04-23 19:42:17 -0500

    The format for steg

--- Steg/steg.py
+++ Steg/steg.py
@@ -7,7 +7,7 @@
 
 #Github link: https://github.com/QueenShorty/CyberStorm/edit/WorkingInProgress/
 
-#Example: python steg.py -(bB) -(sr) -o<val> -w<val> [-h<val>]
+#Example: python steg.py -(bB) -(sr) -o<val> -i[<val>] -w<val> [-h<val>]
 
 #-b Use the bit method
 #-B Use the byte method
@@ -24,22 +24,64 @@ from math import *
 import sys
 from PIL import Image
 
-
-#method = sys.argv[1]
-#data = sys.argv[2]
-#offset = sys.argv[3]
-#interval = sys.argv[4]
-wrapper = sys.argv[1]
-#hidden = sys.argv[6]
-
 #store from left to right : but for cyberstorm might be right to left
 
-with open(wrapper, "rb") as image:
-  f = image.read()
-  b = bytearray(f)
+#with open(wrapper, "rb") as image:
+  #f = image.read()
+  #b = bytearray(f)
   #print b[1]
 
-f = open('/empty.jpg', 'wb')
-f.write(bytearray(b))
-f.close()
+#f = open('/empty.jpg', 'wb')
+#f.write(bytearray(b))
+#f.close()
+
+def format2():
+    offset = sys.argv[3]
+    interval = sys.argv[4]
+    wrapper = sys.argv[5]
+
+    if offset[:2] == "-o":
+        print(offset[2:])
+    else:
+        print("Need -o followed by a number")
+        
+    if interval[:2] == "-i":
+        print(interval[2:])
+    else:
+        print("Need -i followed by a number")
+
+    if wrapper[:2] == "-w":
+        print(wrapper[2:])
+    else:
+        print("Need -w followed by a file name")  
+
+def format():
+    data = sys.argv[2]
+
+    if data == "-s":
+        print("IN -s")
+        hidden = sys.argv[6]
+        format2()
+        
+        if hidden[:2] == "-h":
+            print(hidden[2:])        
+
+    elif data == "-r":
+        print("IN -r")
+        format2()
+
+    else:
+        print("Need -s or -r")
+
+
+method = sys.argv[1]
+
+if method == "-B":
+    print("IN -B")
+    format()
+
+elif method == "-b":
+    print("IN -b")
+    format()
+
                      
