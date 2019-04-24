d352fee53508591aab274f9eb34a093979337b98
Travis-Freese <34549314+Travis-Freese@users.noreply.github.com>
2019-04-24 16:00:23 -0500

    Update chat.py
    Added comments

--- Chat Covert Channel/chat.py
+++ Chat Covert Channel/chat.py
@@ -1,34 +1,52 @@
+# Athenians        #
+# CSC 442/CYEN 301 #
+# Chat Program     #
+# April 24, 2019   #
+####################
+
+#Github link:
+
+
 import socket
 from binascii import unhexlify
 from time import time
 import sys
 
+#Time constants for covert message
 ZERO = 0.025
 ONE = 0.1
 
+#Setup for the socket: IP and port can change
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#ip = '138.47.102.208'
-#port = 31337
-#s.connect((ip, port))
-s.connect(("localhost", 65432))
-data = s.recv(4096)
+ip = "138.47.102.201"
+port = 33333
 
+#Connecting to the server
+s.connect((ip, port))
+#fill a data buffer with 4096 bytes
+data = s.recv(4096)
+#create an array to store the covert message
 covert_bin = ""
+
+#Recives the entire overt message
 while (data.rstrip("\n") != "EOF"):
       sys.stdout.write(data)
       sys.stdout.flush()
+      #calculate delay called delta
       t0 = time()
       data = s.recv(4096)
       t1 = time()
       delta = round(t1-t0, 3)
+      #logic for unveiling the covert message
       if(delta >= ONE):
         covert_bin += "1"
       else:
         covert_bin += "0"
-
 s.close()
+
 covert = ""
 i = 0
+#Processes the covert binary string into ASCII
 while (i < len(covert_bin)):
       #process one byte at a time
     b = covert_bin[i:i + 8]
