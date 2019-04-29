5b8bfc6845c0e34a9f1fff5e5b807a14988049c6
bdoughty <bradforddoughty@gmail.com>
2019-04-29 16:36:21 -0500

    Final version of XOR. Everything should be working correctly

--- XOR Cyrpto/XOR.py
+++ XOR Cyrpto/XOR.py
@@ -11,36 +11,29 @@ import fileinput
 import binascii
         
 def xor():
-        
-        with open("key2", "r") as file:#reads the file, key
-            x = file.read()
+        #If you want to use open up a different key, make sure to change the file name (ex. "key" to "key2" or vice versa)
+        with open("key", "r") as file: #reads the file, key
+             hold = file.read() #Will hold the contents of key
 
+        data = "" #Will hold the contents of the input file
         for line in fileinput.input():#read from stdin to grab ciphertext/plaintext
-            with open("ciphertext2", "r") as file:#reads plaintext
-                data = file.read()
+            data = data + line #Appends each line to the end of data
 
-        message = map(bin, bytearray(data))
-        key = map(bin, bytearray(x))
+        message = map(bin, bytearray(data)) #Converts the contents of the input file to binary numbers
+        key = map(bin, bytearray(hold)) #Converts the contents of the key to binary numbers
 
-        answer = ""
-        for i, _ in enumerate(message):
-            xor = int(message[i], 2) ^ int(key[i], 2)
-            answer = answer + ("{0:08b}".format(xor))
-        print message[0]
-        print key[0]
-        print answer[0:8]
+        answer = "" #Will hold the binary answer after using XOR
+        for i, _ in enumerate(message): #loops through the message
+            indexkey = i % len(key) #Used in case key is shorter than message
+            xor = int(message[i], 2) ^ int(key[indexkey], 2) #Converts each binary number to int, then does XOR
+            answer = answer + ("{0:08b}".format(xor)) #converts new number into binary in 8 number format
 
-        break_up = [(answer[i*8:i*8+8]) for i in range(len(answer)/8)] #Breaks the input binary number into 7s or 8s with the range being the total number of binary numbers divided by 7 or 8
+        break_up = [(answer[i*8:i*8+8]) for i in range(len(answer)/8)] #Breaks the input binary number into 8 so it can be converted to letters
         final = "" #Will hold the final solution
         for i in range(len(break_up)): #Loops through the lenth of break_up, which will be the same length as all the characters
             s = int(break_up[i], 2) #Converts the set of binary numbers into its base 10 numeric form
             final += chr(s) #Converts the numberic number (which is the same as the ASCII code) and converts it back to a character then adds it to the final answer
-        #print final #prints the final decoded message
-        print message[1]
-        print key[1]
-        print answer[8:16]
-        print final[1]
-
+        print final
 xor()
 
                                                                                 
